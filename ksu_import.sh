afd611508868beb1da07f87f78f6e04fedf223b7
balgxmr <jose@pixelos.net>
2024-06-07 23:08:21 -0500

    ksu_import.sh: Update script

--- ksu_import.sh
+++ ksu_import.sh
@@ -3,19 +3,25 @@
 echo Importing KSU...
 
 KSU_DIR=drivers/staging/kernelsu
-if [ -d "$KSU_DIR" ];
-then
+if [ -d "$KSU_DIR" ]; then
     echo "$KSU_DIR directory exists. Removing it!"
     rm -rf drivers/staging/kernelsu
-    echo Commit changes and re-run the script
+    echo "Commit changes and re-run the script"
 else
-    if git ls-remote --exit-code kernelsu; 
-    then
-    	git read-tree --prefix=drivers/staging/kernelsu/ -u kernelsu/main
+    if git ls-remote --exit-code kernelsu; then
+        echo "kernelsu remote exists. Fetching..."
+        git fetch kernelsu
     else
-    	git remote add kernelsu https://github.com/tiann/KernelSU.git
-	git fetch kernelsu
-	git read-tree --prefix=drivers/staging/kernelsu/ -u kernelsu/main
+        echo "Adding kernelsu remote..."
+        git remote add kernelsu https://github.com/tiann/KernelSU.git
+        git fetch kernelsu
     fi
-    echo Done. Now commit changes.
+
+    if git ls-remote --heads kernelsu main; then
+        git read-tree --prefix=drivers/staging/kernelsu/ -u kernelsu/main
+        echo "Done. Now commit changes."
+    else
+        echo "Error: Branch 'main' does not exist in remote 'kernelsu'."
+    fi
 fi
+
